21c29fe28bee727135ba0a1b78b02cee94a30b39
MohitSuthar <mohitsuthar5006@gmail.com>
2026-07-21 12:29:45 +0530

    initialized the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='bg-red-400'>hello world</div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/EnterUser.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EnterUser = () => {
+
+    
+  return (
+    <div>EnterUser</div>
+  )
+}
+
+export default EnterUser
