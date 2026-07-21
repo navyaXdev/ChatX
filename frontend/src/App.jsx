1ec746c936b9fb0c3dc08977e65d477bd987ce2a
MohitSuthar <mohitsuthar5006@gmail.com>
2026-07-21 18:01:46 +0530

    Added homePage for the website

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,17 @@ import './App.css'
 import EnterUser from './pages/EnterUser'
 import ChatPage from './pages/ChatPage'
 import ProtectedRoute from './route/ProtectedRoute'
+import MainPage from './pages/MainPage'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<EnterUser />} />
+        <Route path='/start' element={<EnterUser />} />
+        <Route path='/' element={<MainPage/>}  />
 
         <Route path='/chat' element={
-
           <ProtectedRoute>
             <ChatPage />
           </ProtectedRoute>
